bcace28a0abc3ccc1ecd0b35e476f4013e4bdff8
Zachary Smith <billyzacsmith@gmail.com>
2016-01-29 10:12:45 -0700

    Initial commit. Got the server and server side socket framed out.

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var app = express()
+
+var server = require('http').Server(app)
+
+var io = require('socket.io')(server)
+
+app.use(express.static(__dirname + '/public'))
+
+io.on('connection', function(socket) {
+  var score = {
+    size: {
+      min: 0,
+      max: 100,
+      value: 0,
+      update: update
+    }
+  }
+  socket.on('vote', function(vote) {
+    var response = Object.keys(vote).reduce(function(response, key) {
+      score[key].update(vote[key])
+      response[key] = score[key].value
+      return response
+    }, {})
+    io.emit('score', response)
+  })
+})
+
+function updateValue(min, max, value, vote) {
+  if (value + vote >= min && value + vote <= max) {
+    return value + vote
+  } else {
+    return value
+  }
+}
+
+function update(vote) {
+  this.value = updateValue(this.min, this.max, this.value, vote)
+}
